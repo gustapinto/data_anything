b01d03cf1715048711a7b1fa4d1826b1bb8845e9
ghp2201 <ghp2201@gmail.com>
2020-09-03 08:33:29 -0300

    Reformulate the entire repo structure

---
+++ strava/modules/data_processors.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+dataframe = pd.read_json('strava/datasets/bike_rides.json')
+
+def get_line_average(field):
+    return f'{dataframe.loc[field].mean(axis=0):0.02f}'
+
+def print_average_data():
+    print(f'''
+    Quilometragem média: {get_line_average('mileage')} Km
+    Tempo médio por pedalada: {get_line_average('time')} m:s
+    Velocidade normal média: {get_line_average('average_speed')} Km/h
+    Velocidadade máxima média: {get_line_average('top_speed')} Km/h
+    ''')
+
+def plot_chart(field, title, kind):
+    return dataframe.loc[field].plot(kind=kind, title=title)
--- utils.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-
-def get_line_average(dataframe, field):
-    return dataframe.loc[field].mean(axis=0)
