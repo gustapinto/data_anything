f0c8016d68d82dd4371a93c665c481d6743ea43f
Gustavo Henrique <gustavo.henrique@nappsolutions.com>
2022-11-02 14:55:44 -0300

    Migrate project to use a local notebook-centric aproach instead of jupyterlab

--- docker/dev/jupyterlab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Starts Jupyter without the need of a password or token
-jupyter lab \
-    --allow-root \
-    --ip=0.0.0.0 \
-    --no-browser \
-    --NotebookApp.password='' \
-    --NotebookApp.token=''
--- docker/prod/jupyterlab.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-jupyter lab --ip=0.0.0.0 --allow-root --no-browser
